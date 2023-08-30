8565c3de4ee36e469e46ffc31aa502fae1c0ce2b
klei22 <kauna@avalonwest.tech>
2023-08-30 02:05:09 -0700

    Add positional arguments and tempo feature

--- to_midi.py
+++ to_midi.py
@@ -2,13 +2,23 @@ import mido
 from mido import MidiFile, MidiTrack, Message
 from mido import MetaMessage
 import csv
+import os
+import argparse
 
-def csv_to_midi(csv_file_path, midi_file_path):
+def csv_to_midi(csv_file_path, midi_file_path, tempo_file_path=None):
     mid = MidiFile()
     track = MidiTrack()
     mid.tracks.append(track)
 
-    track.append(MetaMessage('set_tempo', tempo=1000000))
+    # Default tempo
+    tempo = 1000000
+
+    # If a tempo file is specified, use that
+    if tempo_file_path and os.path.exists(tempo_file_path):
+        with open(tempo_file_path, 'r') as tempo_file:
+            tempo = int(tempo_file.read().strip())
+
+    track.append(MetaMessage('set_tempo', tempo=tempo))
     events = []
 
     with open(csv_file_path, 'r') as csvfile:
@@ -44,5 +54,15 @@ def csv_to_midi(csv_file_path, midi_file_path):
     print(f"Converted {csv_file_path} to {midi_file_path}")
 
 if __name__ == "__main__":
-    csv_to_midi('output.csv', 'from_csv.mid')
+    parser = argparse.ArgumentParser(description="Convert a CSV representation of MIDI to an actual MIDI file.")
+    parser.add_argument('-c', required=True, help="Input CSV file path.")
+    parser.add_argument('-m', default=None, help="Output MIDI file path. If not specified, appends .mid to the CSV filename.")
+    parser.add_argument('-t', default=None, help="Optional tempo file path.")
+    
+    args = parser.parse_args()
+
+    if not args.m:
+        args.m = args.c + ".mid"
+    
+    csv_to_midi(args.c, args.m, args.t)
 
